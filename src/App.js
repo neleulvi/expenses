954e465b944641381a6cf7b48bf92cc4db085805
Nele Ulvi <nele.ulvi@voco.ee>
2024-11-22 11:13:04 +0200

    Expense item data props

--- src/App.js
+++ src/App.js
@@ -2,10 +2,42 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem.js';
 
 function App() {
+
+//const date =new Date(2024,11,12)
+//const title = 'New book'
+//const price = 30.99
+// aga ei pea nii pikalt, tee neist üks objekt:data ja siis muuda returnis ka õigeks ja saadab korraga
+//data on nüüd objekt, kasuta loogilisi sulge, sp komad, et on nimi ja väärtus
+//mitme objekti ja expense tegemiseks pane anmed massiivi, massivis eraldad komadega
+const expenses = [
+  {
+  date:new Date(2024,11,12),
+  title: 'New book',
+  price: 30.99,
+},
+{
+  date:new Date(2024,11,12),
+  title: 'Bag',
+  price: 130.99,
+},
+{
+  date:new Date(2024,11,12),
+  title: 'Shoes',
+  price: 230.99,
+}
+]
+//siin on su määratud propsid date, title, price
   return (
     <div className="App">
-      <ExpenseItem/>
-      <ExpenseItem/>
+      <ExpenseItem
+        data={expenses[0]}
+          />
+            <ExpenseItem
+              data={expenses[1]}
+              />
+            <ExpenseItem
+              data={expenses[2]}
+              />
 
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,20 @@
 //funkstioon nimeta sama nimega, mis fail selle sisu määramiseks
 import './ExpenseItem.css' //impordi css faili expenseitem kujuduse jaoks
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
     //javascriptis css esile kutsuimiseks pane className mitte lihtsalt class
     //returni sulgudesse alguse ja lõpu märgend, aalati fikseeri sulgudega
-return (
+console.log(props.data)
+//muutujad ja arvutused enne returni, return juba lükkab andmed faili ja annab tulemusi
+//kasuta loogilisi sulge, et võtaks muutujat
+//props aka propery-atribuut, komponendi atribuut
+//propsi esile kutsumiseks on vajalik props
+    return (
     <div className='expense-item'> 
-        <div>Date</div>
+        <div>{props.data.date.toString()}</div>
         <div className='expense-item__description'>
-            <h2>Title</h2>
-            <div className='expense-item__price'>Price</div>
+            <h2>{props.data.title}</h2>
+            <div className='expense-item__price'>{props.data.price}</div>
         </div>
     </div>
 )
